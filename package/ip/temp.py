593691e3f07db52d85d206217d44677750246501
vikingar <423141759@qq.com>
2023-06-10 12:30:28 +0800

    ip scan

--- package/ip/temp.py
+++ package/ip/temp.py
@@ -1,4 +1,30 @@
-ip = "192.168.0.0"
-with open('ip.txt', 'a+') as f:
-    f.write(str(ip))
-f.close()
+import socket
+
+
+def scan_port(ip, port):
+    try:
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.settimeout(0.01)
+
+        result = sock.connect_ex((ip, port))
+
+        if result == 0:
+            print(f"Port {port} is open on {ip}")
+            with open('ip.txt', 'a+') as f:
+                f.write(ip)
+
+        sock.close()
+    except socket.error:
+        print(f"Could not connect to {ip}")
+
+
+port = 80
+arr = [8, 198]
+for i in arr:
+    for j in range(0, 255):
+        for k in range(0, 255):
+            for m in range(0, 255):
+                ip = f"{i}.{j}.{k}.{m}"
+                scan_port(ip, port)
