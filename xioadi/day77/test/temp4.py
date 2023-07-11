e3832b8f2ccd3c6300845b7e2bf5ce8b48819fd8
vikingar <423141759@qq.com>
2023-07-11 19:52:37 +0800

    temp and so on

--- xioadi/day77/test/temp4.py
+++ xioadi/day77/test/temp4.py
@@ -1,6 +1,6 @@
 def deleteDupLine(input_file, output_file):
     ip_set = set()
-    with open(input_file, 'r') as f:
+    with open(input_file, 'r', encoding='utf-8') as f:
         line = f.readline()
         while line:
             line = line.strip()
@@ -8,14 +8,13 @@ def deleteDupLine(input_file, output_file):
                 ip_set.add(line)
             line = f.readline()
 
-
-    with open(output_file, 'w') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         for item in ip_set:
             f.write(item + '\n')
     f.close()
 
 
 if __name__ == '__main__':
-    input_file = '../fofa/glassfish_fofa_scan/input.txt'
-    output_file = '../fofa/glassfish_fofa_scan/output.txt'
+    input_file = 'school.txt'
+    output_file = 'output.txt'
     deleteDupLine(input_file, output_file)
