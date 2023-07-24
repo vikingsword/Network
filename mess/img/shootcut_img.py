c2aba97a9c36e4c6026d7229adb769e828b6d600
vikingar <423141759@qq.com>
2023-07-24 20:37:20 +0800

    practices

---
+++ mess/img/shootcut_img.py
@@ -0,0 +1,7 @@
+from PIL import ImageGrab
+
+# 截取整个屏幕
+im = ImageGrab.grab()
+
+# 显示截图
+im.show()
--- scan/nmap/nmap_test1.py
+++ scan/nmap/nmap_test1.py
@@ -1,10 +1,19 @@
-import nmap3
+import nmap
 
 
+def scan(host):
+    nm = nmap.PortScanner()
+    nm.scan(hosts=host, arguments='-p 1-1000')  # 你可以在arguments参数中设置Nmap的选项和参数
 
-nmap = nmap3.Nmap()
-results = nmap.nmap_version()
-# print(results)
+    for host in nm.all_hosts():
+        print(f'Host: {host}')
+        for proto in nm[host].all_protocols():
+            print(f'Protocol: {proto}')
+            ports = nm[host][proto].keys()
+            for port in ports:
+                print(f'Port: {port}, State: {nm[host][proto][port]["state"]}')
 
 
-
+if __name__ == "__main__":
+    target_host = "127.0.0.1"  # 你的目标主机，可以是单个IP地址或者IP段
+    scan(target_host)
