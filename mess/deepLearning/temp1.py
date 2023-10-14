d6e8dc4efc74a6ced167e7d94f1181c9dd3eafd3
vikingar <423141759@qq.com>
2023-10-14 23:05:22 +0800

    practice

--- mess/deepLearning/temp1.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-version = tf.__version__
-# pu_ok = tf.test.is_gpu_available()
-gpu_ok = tf.config.list_physical_devices("GPU")
-print("tf version:", version, "\nGPU number", gpu_ok)
