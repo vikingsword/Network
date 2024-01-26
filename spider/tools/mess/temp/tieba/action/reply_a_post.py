df0805cc78a9bb12eb75ed9df4241f03da108be7
vikingar <423141759@qq.com>
2024-01-26 16:46:08 +0800

    spider

---
+++ spider/tools/mess/temp/tieba/action/reply_a_post.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+def reply_a_post(driver, url, content):
+    post_url = url
+    driver.get(post_url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+
+    # 点击回复框
+    element = driver.find_element(By.XPATH, '//div[@id="rich_ueditor_tpl"]//div[@class="old_style_wrapper"]')
+    action = ActionChains(driver)
+    action.move_to_element_with_offset(element, 50, 50)
+    action.click()
+    action.send_keys(content)
+    action.perform()
+
+    # 点击发表 reply post
+    element2 = driver.find_element(By.XPATH, '//div[@id="tb_rich_poster"]//div[@class="j_floating"]//span')
+    action.move_to_element_with_offset(element2, 3, 3)
+    action.click()
+    action.perform()
--- spider/tools/mess/temp/tieba/main.py
+++ spider/tools/mess/temp/tieba/main.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from login import login
+from action.reply_a_post import reply_a_post
 
 
 
@@ -17,24 +18,13 @@ if __name__ == '__main__':
     # 登录
     login(driver)
 
+    content = 'hello3'
     post_url = 'https://tieba.baidu.com/p/8875880579'
-    driver.get(post_url)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
-    element = driver.find_element(By.XPATH, '//div[@id="rich_ueditor_tpl"]//div[@class="old_style_wrapper"]')
-    action = ActionChains(driver)
-    action.move_to_element_with_offset(element, 50, 50)
-    action.click()
-    action.send_keys('hello2')
-    action.perform()
+    reply_a_post(driver, post_url, content)
 
-    element2 = driver.find_element(By.XPATH, '//div[@id="tb_rich_poster"]//div[@class="j_floating"]//span')
-    action.move_to_element_with_offset(element2, 3, 3)
-    action.click()
-    action.perform()
-
-
-    time.sleep(30)
+    time.sleep(20)
     # 防止自动退出
     input()
     driver.close()
+
+
