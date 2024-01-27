382ab1722124a26cc1a1ac1a2a60a69d9d4c1b63
vikingar <423141759@qq.com>
2024-01-27 10:14:11 +0800

    thread pool

--- spider/tools/anime/newAanime.py
+++ spider/tools/anime/newAanime.py
@@ -3,24 +3,50 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from lxml import etree
 
 url = 'https://www.ntdm9.com/play/5534-1-4.html'
 
-# no headers
-edge_options = webdriver.EdgeOptions()
-edge_options.add_argument("--headless")
-driver = webdriver.Edge(options=edge_options)
+# # no headers
+# edge_options = webdriver.EdgeOptions()
+# edge_options.add_argument("--headless")
+# driver = webdriver.Edge(options=edge_options)
+
+# headers
+driver = webdriver.Edge()
+
+
 driver.implicitly_wait(30)
-
 driver.get(url)
-page_source = driver.page_source
+driver.maximize_window()
 
-tree = etree.HTML(page_source)
+# 获取浏览器窗口的大小
+window_size = driver.get_window_size()
 
-element = driver.find_element(By.TAG_NAME, 'video').get_attribute('src')
-print(element)
+# 设置要点击的位置（这里以浏览器窗口中心为例）
+click_x, click_y = window_size['width'] // 2, window_size['height'] // 2
 
-time.sleep(10)
+# 创建 ActionChains 对象
+actions = ActionChains(driver)
+
+# 移动到浏览器窗口的特定位置并点击
+actions.move_by_offset(click_x, click_y)
+time.sleep(3)
+actions.click()
+actions.perform()
+
+# 等待视频元素加载完成
+video_element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.TAG_NAME, 'video'))
+)
+# 获取视频文件地址
+video_source = video_element.get_attribute('src')
+# 打印视频文件地址
+print(f"Video source: {video_source}")
+
+input()
 driver.close()
