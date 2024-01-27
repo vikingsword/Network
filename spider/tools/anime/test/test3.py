544375aa2760071b177eb2ed665a3d9c02462a34
vikingar <423141759@qq.com>
2024-01-27 12:14:50 +0800

    anime spider

--- spider/tools/anime/test/test3.py
+++ spider/tools/anime/test/test3.py
@@ -21,11 +21,25 @@ driver = webdriver.Edge()
 
 driver.maximize_window()
 
+# 打开网站
 driver.get(url)
 
-time.sleep(5)
-page_source = driver.page_source
-print(page_source)
+# 切换到第一个 iframe，你可以根据实际情况修改索引或使用其他定位方法
+first_iframe_element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.TAG_NAME, 'iframe'))
+)
+driver.switch_to.frame(first_iframe_element)
 
+# 切换到包含 #document 的 iframe
+document_iframe_element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '#document'))
+)
+driver.switch_to.frame(document_iframe_element)
+
+# 在 #document 中执行操作，例如获取源码
+document_page_source = driver.page_source
+
+# 打印 #document 中的源码
+print(document_page_source)
 input()
 driver.close()
