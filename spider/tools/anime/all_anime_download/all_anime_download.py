1364df3a0ec4ecbf2d3cd34a7e4ac72aa0a54eaf
vikingar <423141759@qq.com>
2024-01-28 17:57:48 +0800

    anime spider

--- spider/tools/anime/all_anime_download/all_anime_download.py
+++ spider/tools/anime/all_anime_download/all_anime_download.py
@@ -12,19 +12,34 @@ def init_driver():
     driver.maximize_window()
     return driver
 
+
+
+
 def get_page_urls(driver):
-    base_url = 'https://www.ntdm9.com/'
+    base_url = 'https://www.ntdm9.com'
     with open('anime_homepage.txt', 'r', encoding='utf-8') as f:
         for url in f:
             url = url.strip()
             print("home_url = ", url)
             driver.get(url)
             page_source = driver.page_source
-            tree = etree.HTML(page_source)
-            li_list = tree.xpath('//div[@id="main0"]//ul/li')
+            # get anime title
+            tree1 = etree.HTML(page_source)
+            anime_title = tree1.xpath('//div[@class="blockcontent"]//h4[@class="detail_imform_name"]/text()')[0]
+            print("anime_title = ", anime_title)
+            # get every episode url
+            tree2 = etree.HTML(page_source)
+            li_list = tree2.xpath('//div[@id="main0"]//ul/li')
             for li in li_list:
                 href = li.xpath('./a/@href')[0]
-                print("episode_url = ", base_url + href)
+                episode_url = base_url + href
+                print("episode_url = ", episode_url)
+                # episode_download(episode_url)
+
+
+def episode_download(url):
+    driver = init_driver()
+    driver.get(url)
 
 
 
