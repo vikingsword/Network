fbc603f424be9cb3800be02541c8ff74c39c398b
vikingar <423141759@qq.com>
2024-01-31 16:47:39 +0800

    temp

--- spider/tools/tieba/tieba2/reply/action/reply_content.py
+++ spider/tools/tieba/tieba2/reply/action/reply_content.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 
-def reply_a_post(driver, url, content):
+def reply_text(driver, url, content):
     post_url = url
     driver.get(post_url)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -19,8 +19,24 @@ def reply_a_post(driver, url, content):
     action.send_keys(content)
     action.perform()
 
-    # 点击发表 reply post
+    # 点击发表 reply text post
     element2 = driver.find_element(By.XPATH, '//div[@id="tb_rich_poster"]//div[@class="j_floating"]//span')
     action.move_to_element_with_offset(element2, 3, 3)
     action.click()
     action.perform()
+
+
+def reply_img(driver, url):
+    post_url = url
+    driver.get(post_url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+
+    # 插入图片
+    element = driver.find_element(By.XPATH,
+                                  '//div[@class="edui-btn-toolbar"]//div[@class="edui-btn edui-btn-image edui-btn-name-list"]//div[@class="edui-icon-image edui-icon"]')
+    action = ActionChains(driver)
+    action.move_to_element_with_offset(element, 1, 1)
+    action.click()
+    action.perform()
+    print()
--- spider/tools/tieba/tieba2/reply/main.py
+++ spider/tools/tieba/tieba2/reply/main.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import time
-
 from selenium import webdriver
-
-from action.reply_a_post import reply_a_post
+from action.reply_content import reply_text
+from action.reply_content import reply_img
 from login import login
 
 if __name__ == '__main__':
@@ -16,13 +15,17 @@ if __name__ == '__main__':
     # 登录
     login(driver)
 
-    content = 'hello3'
+    # # 发送文本
+    # content = 'hello3'
+    # post_url = 'https://tieba.baidu.com/p/8875880579'
+    # reply_text(driver, post_url, content)
+
+    # 发送图片
     post_url = 'https://tieba.baidu.com/p/8875880579'
-    reply_a_post(driver, post_url, content)
+    reply_img(driver, post_url)
+
 
     time.sleep(20)
     # 防止自动退出
     input()
     driver.close()
-
-
