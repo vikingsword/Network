31d90012898fbabc4213b88b3a059e16eb64af8d
vikingar <423141759@qq.com>
2024-02-03 17:28:50 +0800

    check repetition

---
+++ tools/file_tools/check_repetition.py
@@ -0,0 +1,16 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+
+file_path = 'E://Dev//Python//Project//Network//spider//tools//anime//new_anime_download//anime_homepage.txt'
+list = []
+count = 0
+for line in open(file_path, 'r'):
+    for item in list:
+        if item == line:
+            print('有重复元素： ', line)
+            break
+    list.append(line.strip())
+    count += 1
+
+if list.__len__() == count:
+    print('没有重复元素')
