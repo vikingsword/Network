bc419b75a2339ab06bc5d00e58a5250509520e4b
vikingar <423141759@qq.com>
2024-02-04 18:45:57 +0800

    get new anime v2.0

--- spider/tools/anime/new_anime_download/v02/download_anime.py
+++ spider/tools/anime/new_anime_download/v02/download_anime.py
@@ -4,7 +4,7 @@ import time
 import requests
 
 from get_anime_list import get_episode
-from get_anime_list import get_anime_len
+from get_anime_list import get_list_len
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from lxml import etree
@@ -32,6 +32,7 @@ def download_detail(filename, url):
 
         # 以二进制写入文件
         path = save_path + filename + ".mp4"
+        print('start download: ', filename)
         with open(path, 'wb') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
@@ -43,18 +44,23 @@ def download_detail(filename, url):
 
 
 def download_handler(filename, url):
+    time.sleep(1)
     driver2 = init_driver()
     driver2.get(url)
-    print("url = ", url)
     td_element = WebDriverWait(driver2, 30).until(
         EC.presence_of_element_located((By.XPATH, '//table//td[@id="playleft"]'))
     )
+    # print(driver2.page_source)
 
     iframe_element = td_element.find_element(By.TAG_NAME, 'iframe')
+    # todo : 不知道为什么有的时候print一下就可以找到iframe,有的时候不用print就可以
+    # print('switch to frame...')
 
     driver2.switch_to.frame(iframe_element)
+    time.sleep(0.5)
     iframe_page_source = driver2.page_source
     tree = etree.HTML(iframe_page_source)
+    # print(iframe_page_source)
     anime_src = tree.xpath('//video[@id="lelevideo"]/@src')[0]
     if anime_src is not None:
         with open('anime_src.txt', 'w', encoding='utf-8') as f:
@@ -82,6 +88,7 @@ def save_download(file_name, url):
     else:
         # 如果循环正常结束（未被 break 中断），表示重试次数达到上限
         print("Max retries reached, giving up.")
+
 
 
 def download_anime():
@@ -112,7 +119,7 @@ if __name__ == '__main__':
 
     driver = init_driver()
 
-    if get_anime_len == 0:
+    if get_list_len == 0:
         flag = get_episode(driver=driver)
     else:
         download_anime()
--- spider/tools/anime/new_anime_download/v02/test.py
+++ spider/tools/anime/new_anime_download/v02/test.py
@@ -1,6 +1,10 @@
 # !usr/bin/env python
 # -*- coding:utf-8 _*-
 
-for line in open('anime_homepage.txt', 'r', encoding='utf-8'):
-    line = line.strip()
-    print(line)
+from download_anime import init_driver
+
+driver = init_driver()
+
+driver.get('https://www.ntdm9.com/play/5160-1-17.html')
+res = driver.page_source
+print(res)
