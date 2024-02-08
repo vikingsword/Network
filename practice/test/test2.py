a0cf3aac00fc1fcc6ae461303519f8790ff5e769
vikingar <423141759@qq.com>
2024-02-08 17:02:28 +0800

    test

--- practice/test/test2.py
+++ practice/test/test2.py
@@ -7,6 +7,9 @@ class Driver:
     def __init__(self):
         self.driver = webdriver.Edge()
 
+    def get(self, url):
+        self.driver.get(url)
+
 
 if __name__ == '__main__':
     driver = Driver()
